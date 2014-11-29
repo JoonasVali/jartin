da550867e28d89163003978c401666baba2973b9
Joonas Vali <joonas.vali@zeroturnaround.com>
2014-11-29 19:24:21 +0200

    added stamp groups

--- src/main/java/ee/joonasvali/stamps/Main.java
+++ src/main/java/ee/joonasvali/stamps/Main.java
@@ -1,15 +1,16 @@
 package ee.joonasvali.stamps;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Main {
-  static Stamps stamps = new Stamps(new File(Main.class.getResource("/stamps").getFile()));
+  static GroupedStamps stampPool = new GroupedStamps(new File(Main.class.getResource("/stamps").getFile()));
   public static void main(String[] args) {
+
+    Stamps stamps = generateNewStamps();
     BufferedImage canvas = init(stamps);
     JFrame frame = new JFrame("LOL");
     frame.getContentPane().add(new JLabel(new ImageIcon(canvas)));
@@ -24,8 +25,8 @@ public class Main {
       @Override
       public void keyReleased(KeyEvent e) {
         if(KeyEvent.VK_SPACE == e.getKeyCode()) {
-          Stamp.clearCache();
-          BufferedImage img = init(stamps);
+          clearCaches();
+          BufferedImage img = init(generateNewStamps());
           frame.getContentPane().removeAll();
           frame.getContentPane().add(new JLabel(new ImageIcon(img)));
           frame.pack();
@@ -34,24 +35,32 @@ public class Main {
     });
     frame.setVisible(true);
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+  }
 
+  private static void clearCaches() {
+    Stamp.clearCache();
+    stampPool.clearCaches();
+  }
+
+  private static Stamps generateNewStamps() {
+    return stampPool.getStamps(5, 3, new RandomQuery<>(), new RandomQuery<>(), false);
   }
 
   private static BufferedImage init(Stamps stamps) {
-    int x = 1300;
-    int y = 600;
+    //int x = 1300;
+    //int y = 600;
+    int x = 1900;
+    int y = 1000;
     CompositeStamps compositeStamps = new CompositeStamps(stamps, new RandomComposerStrategy(5));
-    StampPallette stampPallette = new StampPallette(compositeStamps, 8);
-    RandomQuery<Stamp> ran = new RandomQuery<>();
-    RandomQuery<Color> ranColor = new RandomQuery<>();
     Pallette pallette = new Pallette(3);
 
-    ProjectionGenerator gen = new ProjectionGenerator(x, y, stampPallette, pallette);
+    ProjectionGenerator gen = new ProjectionGenerator(x, y, stamps, pallette);
 
-    Painting painting = new Painting(x, y);
-    JFrame frame = new JFrame("LOL");
+    Painting painting = new Painting(x, y, pallette);
 
-    int projections = (int) (x * y / 350d);
+    int projections = (int) (x * y / 500d);
+
+    System.out.println(projections);
     for (int i = 0; i < projections; i++) {
       painting.addProjection(gen.generate());
     }
