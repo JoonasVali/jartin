cdb5ede62e0fb2bba429830081606ac2202f8382
Joonas Vali <joonas.wali@gmail.com>
2014-12-21 12:52:49 +0200

    PReferences field validation added

--- src/main/java/ee/joonasvali/stamps/ui/PreferencesPanel.java
+++ src/main/java/ee/joonasvali/stamps/ui/PreferencesPanel.java
@@ -3,6 +3,8 @@ package ee.joonasvali.stamps.ui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -13,6 +15,12 @@ public class PreferencesPanel extends JPanel {
   private Preferences preferences;
   private JButton save = new JButton("Save");
   private ActionListener closeAction;
+
+  private JLabel errors = new JLabel();
+  private List<Validator> validatorList = new LinkedList<Validator>();
+  private List<ActionListener> onSave = new LinkedList<>();
+
+
   public PreferencesPanel(Preferences preferences, ActionListener closeAction) {
     this.closeAction = closeAction;
     this.preferences = preferences;
@@ -21,27 +29,62 @@ public class PreferencesPanel extends JPanel {
 
   public void init() {
     this.setLayout(new GridLayout(0, 1));
-    this.add(createInputField("Width", preferences::getWidth, preferences::setWidth));
-    this.add(createInputField("Height", preferences::getHeight, preferences::setHeight));
-    this.add(createInputField("Base colors", preferences::getNumberOfColors, preferences::setNumberOfColors));
-    this.add(createInputField("Stamp count demultiplier", preferences::getStampCountDemultiplier, preferences::setStampCountDemultiplier));
-    this.add(createInputField("Stamp groups count", preferences::getStampGroupsCount, preferences::setStampGroupsCount));
-    this.add(createInputField("Stamps per group", preferences::getStampsPerGroup, preferences::setStampsPerGroup));
-    save.addActionListener(s -> closeAction.actionPerformed(null));
+    this.add(errors);
+    create("Width", preferences::getWidth, preferences::setWidth, 100, Integer.MAX_VALUE);
+    create("Height", preferences::getHeight, preferences::setHeight, 100, Integer.MAX_VALUE);
+    create("Base colors", preferences::getNumberOfColors, preferences::setNumberOfColors, 2, 50);
+    create("Stamp count demultiplier", preferences::getStampCountDemultiplier, preferences::setStampCountDemultiplier, 100, 6000);
+    create("Stamp groups count", preferences::getStampGroupsCount, preferences::setStampGroupsCount, 1, 100);
+    create("Stamps per group", preferences::getStampsPerGroup, preferences::setStampsPerGroup, 1, 100);
+
+    save.addActionListener(s -> {
+      String errors = validateFields();
+      if (errors.isEmpty()) {
+        closeAction.actionPerformed(null);
+        for(ActionListener listener : onSave) {
+          listener.actionPerformed(s);
+        }
+      } else {
+        this.errors.setForeground(Color.RED);
+        this.errors.setText(errors);
+      }
+    });
 
     JPanel savePanel = new JPanel(new FlowLayout());
     savePanel.add(save);
     this.add(savePanel);
   }
 
-  private JComponent createInputField(String name, Supplier<Integer> supplier, Consumer<Integer> consumer) {
+  private void create(String name, Supplier<Integer> supplier, Consumer<Integer> consumer, int minValue, int maxValue) {
+    JComponent component = createInputField(name, supplier, consumer, minValue, maxValue);
+    this.add(component);
+  }
+
+  private String validateFields() {
+    for(Validator validator : validatorList) {
+      String error = validator.validate();
+      if (!error.isEmpty()) {
+        return error;
+      }
+    }
+    return "";
+  }
+
+  private JComponent createInputField(String name, Supplier<Integer> supplier, Consumer<Integer> consumer, int minValue, int maxValue) {
     JTextField field = new JTextField(String.valueOf(supplier.get()), 5);
+    validatorList.add(new IntegerValidator(field, minValue, maxValue, name));
+
     JPanel panel = new JPanel(new FlowLayout());
     panel.add(new JLabel(name));
     panel.add(field);
-    save.addActionListener(s -> consumer.accept(Integer.parseInt(field.getText())));
+    onSave.add(s -> {
+      try {
+        consumer.accept(Integer.parseInt(field.getText()));
+      }
+      catch (Exception e) {
+        errors.setText(errors.getText() + "\n" + name + " " + e.getMessage());
+      }
+    });
     return panel;
   }
-
-
 }
