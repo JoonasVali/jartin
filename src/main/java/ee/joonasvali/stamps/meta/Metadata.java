a8b623d2a70cd7311803fecb78415329f8873756
Joonas Vali <joonas.vali@zeroturnaround.com>
2015-02-22 22:07:25 +0200

    moved some metadata from java code to pom.xml

--- src/main/java/ee/joonasvali/stamps/meta/Metadata.java
+++ src/main/java/ee/joonasvali/stamps/meta/Metadata.java
@@ -1,8 +1,56 @@
 package ee.joonasvali.stamps.meta;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @author Joonas Vali
  */
 public class Metadata {
-  public final static String VERSION = "Jartin 1.0.alpha2";
+  private static final String VERSION_KEY = "project.version";
+  private static final String NAME_KEY = "project.name";
+  public static final Metadata INSTANCE = new Metadata();
+  public final Logger log = LoggerFactory.getLogger(Metadata.class);
+
+  public final String VERSION;
+  public final String NAME;
+
+
+
+
+  public Metadata() {
+    File file = new File(ClassLoader.class.getResource("/meta.properties").getFile());
+    Properties properties = new Properties();
+    try (InputStream inputStream = new FileInputStream(file)) {
+      properties.load(inputStream);
+    } catch (IOException e) {
+      log.error(e.getMessage(), e);
+      System.exit(-1);
+    }
+
+    String version = properties.getProperty(VERSION_KEY);
+    String name = properties.getProperty(NAME_KEY);
+
+    if(version == null || name == null) {
+      log.error("name or version == null | name = " + name + ", version = " + version);
+      System.exit(-1);
+    }
+
+    VERSION = version;
+    NAME = name;
+  }
+
+  public String getVersion() {
+    return VERSION;
+  }
+
+  public String getName() {
+    return NAME;
+  }
 }
