719bde2b12c8baf4cff44b2ac9f1e5e286dae715
Joonas Vali <joonas.vali@zeroturnaround.com>
2015-02-28 16:56:57 +0200

    maven build timestamp added

--- src/main/java/ee/joonasvali/stamps/meta/Metadata.java
+++ src/main/java/ee/joonasvali/stamps/meta/Metadata.java
@@ -7,6 +7,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -16,6 +20,9 @@ public class Metadata {
   private static final String VERSION_KEY = "project.version";
   private static final String NAME_KEY = "project.name";
   private static final String AUTHOR_KEY = "project.author";
+  private static final String TIMESTAMP_KEY = "project.timestamp";
+  private static final String TIMESTAMP_PATTERN_KEY = "project.timestamp.pattern";
+
   private static final String META_PROPERTIES = "/meta/meta.properties";
   public static final Metadata INSTANCE = new Metadata();
   public final Logger log = LoggerFactory.getLogger(Metadata.class);
@@ -23,6 +30,7 @@ public class Metadata {
   public final String VERSION;
   public final String NAME;
   public final String AUTHOR;
+  public final String DATE;
 
   public Metadata() {
     File file = new File(ClassLoader.class.getResource(META_PROPERTIES).getFile());
@@ -37,6 +45,8 @@ public class Metadata {
     String version = properties.getProperty(VERSION_KEY);
     String name = properties.getProperty(NAME_KEY);
     String author = properties.getProperty(AUTHOR_KEY);
+    String timeString = properties.getProperty(TIMESTAMP_KEY);
+    String timePattern = properties.getProperty(TIMESTAMP_PATTERN_KEY);
 
     if(version == null || name == null) {
       log.error("name or version == null | name = " + name + ", version = " + version);
@@ -46,6 +56,23 @@ public class Metadata {
     AUTHOR = author;
     VERSION = version;
     NAME = name;
+    DATE = parseYear(timeString, timePattern);
+  }
+
+  private String parseYear(String timeString, String timePattern) {
+    DateFormat parser = new SimpleDateFormat(timePattern);
+    DateFormat yearFormatter = new SimpleDateFormat("yyyy");
+    Date date = null;
+    try {
+      date = parser.parse(timeString);
+    } catch (ParseException e) {
+      log.error("Parsing date failed.", e);
+      log.error("Date: " + date);
+      log.error("Pattern used: " + timePattern);
+      System.exit(-1);
+    }
+
+    return yearFormatter.format(date);
   }
 
   public String getVersion() {
@@ -58,5 +85,9 @@ public class Metadata {
 
   public String getAuthor() {
     return AUTHOR;
+  }
+
+  public String getDate() {
+    return DATE;
   }
 }
