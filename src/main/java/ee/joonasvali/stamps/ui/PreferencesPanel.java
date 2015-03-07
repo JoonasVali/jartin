197931f50b59853c8ef680fa4d3df8ba75a76ccc
Joonas Vali <joonas.wali@gmail.com>
2015-03-07 19:01:04 +0200

    stamp count demultiplier as a jslider

--- src/main/java/ee/joonasvali/stamps/ui/PreferencesPanel.java
+++ src/main/java/ee/joonasvali/stamps/ui/PreferencesPanel.java
@@ -3,6 +3,8 @@ package ee.joonasvali.stamps.ui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -12,6 +14,8 @@ import java.util.function.Supplier;
  * @author Joonas Vali
  */
 public class PreferencesPanel extends JPanel {
+  public static final int MAX_STAMP_DEMULTIPLIER = 20000;
+  public static final int MIN_STAMP_DEMULTIPLIER = 100;
   private final Preferences preferences;
   private final JButton save = new JButton("Save");
   private final ActionListener closeAction;
@@ -19,6 +23,7 @@ public class PreferencesPanel extends JPanel {
   private final JLabel errors = new JLabel();
   private final List<Validator> validatorList = new LinkedList<>();
   private final List<ActionListener> onSave = new LinkedList<>();
+  private Dictionary demultiplierDictionary;
 
 
   public PreferencesPanel(Preferences preferences, ActionListener closeAction) {
@@ -33,7 +38,8 @@ public class PreferencesPanel extends JPanel {
     create("Width", preferences::getWidth, preferences::setWidth, 100, Integer.MAX_VALUE);
     create("Height", preferences::getHeight, preferences::setHeight, 100, Integer.MAX_VALUE);
     create("Base colors", preferences::getNumberOfColors, preferences::setNumberOfColors, 2, 50);
-    create("Stamp count demultiplier", preferences::getStampCountDemultiplier, preferences::setStampCountDemultiplier, 100, 6000);
+    createSlider("Stamp amount", preferences::getStampCountDemultiplier, preferences::setStampCountDemultiplier,
+        getDemultiplierDictionary(MIN_STAMP_DEMULTIPLIER, MAX_STAMP_DEMULTIPLIER), true,  MIN_STAMP_DEMULTIPLIER, MAX_STAMP_DEMULTIPLIER);
     create("Stamp groups count", preferences::getStampGroupsCount, preferences::setStampGroupsCount, 1, 100);
     create("Stamps per group", preferences::getStampsPerGroup, preferences::setStampsPerGroup, 1, 100);
 
@@ -41,7 +47,7 @@ public class PreferencesPanel extends JPanel {
       String errors = validateFields();
       if (errors.isEmpty()) {
         closeAction.actionPerformed(null);
-        for(ActionListener listener : onSave) {
+        for (ActionListener listener : onSave) {
           listener.actionPerformed(s);
         }
       } else {
@@ -55,13 +61,40 @@ public class PreferencesPanel extends JPanel {
     this.add(savePanel);
   }
 
+  private void createSlider(String name, Supplier<Integer> supplier, Consumer<Integer> consumer, Dictionary dictionary, boolean inverted, int minValue, int maxValue) {
+    JComponent component = createSliderObject(name, supplier, consumer, dictionary, inverted, minValue, maxValue);
+    this.add(component);
+  }
+
+  private JComponent createSliderObject(String name, Supplier<Integer> supplier, Consumer<Integer> consumer, Dictionary dictionary, boolean inverted, int minValue, int maxValue) {
+    JSlider slider = new JSlider(minValue, maxValue, supplier.get());
+    slider.setInverted(inverted);
+    if (dictionary != null) {
+      slider.setLabelTable(dictionary);
+      slider.setPaintLabels(true);
+    }
+    validatorList.add(new IntegerValidator(slider::getValue, minValue, maxValue, name));
+
+    JPanel panel = new JPanel(new FlowLayout());
+    panel.add(new JLabel(name));
+    panel.add(slider);
+    onSave.add(s -> {
+      try {
+        consumer.accept(slider.getValue());
+      } catch (Exception e) {
+        errors.setText(errors.getText() + "\n" + name + " " + e.getMessage());
+      }
+    });
+    return panel;
+  }
+
   private void create(String name, Supplier<Integer> supplier, Consumer<Integer> consumer, int minValue, int maxValue) {
     JComponent component = createInputField(name, supplier, consumer, minValue, maxValue);
     this.add(component);
   }
 
   private String validateFields() {
-    for(Validator validator : validatorList) {
+    for (Validator validator : validatorList) {
       String error = validator.validate();
       if (!error.isEmpty()) {
         return error;
@@ -72,7 +105,7 @@ public class PreferencesPanel extends JPanel {
 
   private JComponent createInputField(String name, Supplier<Integer> supplier, Consumer<Integer> consumer, int minValue, int maxValue) {
     JTextField field = new JTextField(String.valueOf(supplier.get()), 5);
-    validatorList.add(new IntegerValidator(field, minValue, maxValue, name));
+    validatorList.add(new IntegerValidator(() -> Integer.parseInt(field.getText()), minValue, maxValue, name));
 
     JPanel panel = new JPanel(new FlowLayout());
     panel.add(new JLabel(name));
@@ -80,11 +113,17 @@ public class PreferencesPanel extends JPanel {
     onSave.add(s -> {
       try {
         consumer.accept(Integer.parseInt(field.getText()));
-      }
-      catch (Exception e) {
+      } catch (Exception e) {
         errors.setText(errors.getText() + "\n" + name + " " + e.getMessage());
       }
     });
     return panel;
+  }
+
+  public Hashtable<Integer, JLabel> getDemultiplierDictionary(int min, int max) {
+    Hashtable<Integer, JLabel> table = new Hashtable<>();
+    table.put(min, new JLabel("more"));
+    table.put(max, new JLabel("less"));
+    return table;
   }
 }
