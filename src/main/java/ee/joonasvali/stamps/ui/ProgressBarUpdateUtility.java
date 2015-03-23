f14c820aee5a53655458977e46f96942a387b9a6
Joonas Vali <joonas.wali@gmail.com>
2015-03-23 21:30:00 +0200

    progresscounter can show messages

--- src/main/java/ee/joonasvali/stamps/ui/ProgressBarUpdateUtility.java
+++ src/main/java/ee/joonasvali/stamps/ui/ProgressBarUpdateUtility.java
@@ -9,24 +9,39 @@ public class ProgressBarUpdateUtility implements ProgressListener {
   private final JProgressBar bar;
   private volatile int value;
   private volatile boolean queued = false;
+  private volatile String message;
 
   public ProgressBarUpdateUtility(JProgressBar bar) {
     this.bar = bar;
   }
 
   private void paintValue() {
-    bar.setValue(value);
+    String mess = message;
+    if (mess != null) {
+      bar.setString(mess);
+    } else {
+      bar.setString(null);
+      bar.setValue(value);
+    }
     bar.repaint();
     queued = false;
   }
 
   @Override
   public synchronized void setValue(int value) {
-    if (queued || this.value == value) {
+    if ((queued || this.value == value) && message == null) {
       return;
     }
+    message = null;
     this.value = value;
     queued = true;
+    SwingUtilities.invokeLater(this::paintValue);
+  }
+
+  @Override
+  public void setValue(String message) {
+    this.message = message;
+    queued = false;
     SwingUtilities.invokeLater(this::paintValue);
   }
 
