82172a1f07b78b94c8f677e34e4e068f4bc87f5a
Joonas Vali <joonas.vali@zeroturnaround.com>
2015-03-31 18:28:45 +0300

    alpha4

--- src/main/java/ee/joonasvali/stamps/DefaultProjectionFactory.java
+++ src/main/java/ee/joonasvali/stamps/DefaultProjectionFactory.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2015, Jartin. All rights reserved. This application is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; This application is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. Do not remove this header.
+ */
+
 package ee.joonasvali.stamps;
 
 import ee.joonasvali.stamps.color.PositionAwareColor;
